b80945bbeebb5f2eb29ab99a6467eb89da69d054
Paul Herzog <pmherzog@gmail.com>
2024-02-05 06:48:00 -0500

    Additional assertions added and jsdocs

--- src/assert/assert.ts
+++ src/assert/assert.ts
@@ -10,25 +10,27 @@ import { type AssertType, AssertInfo } from './tracker'
 
 const was_hit = true
 const must_be_hit = true
-// const optionally_hit = false
+const optionally_hit = false
 const expecting_true = true
 
 const universal_test = 'every'
-// const existential_test = 'some'
-// const reachability_test = 'none'
+const existential_test = 'some'
+const reachability_test = 'none'
 
-/** Assert that condition is true one or more times during a test. Callers of
- * `Always` can see failures in two cases:
- * 1. If this function is ever invoked with a `false` for the conditional or
- * 2. If an "indexed" invocation of Always is not covered at least once.
+/**
+ * Assert that condition is true every time this function is called,
+ * AND that it is called at least once. This test property will be
+ * viewable in the "Antithesis SDK: Always" group of the triage report.
  *
- * @param message will be used as a display name in reporting and should therefore be
- * useful to a broad audience.
+ * @param {string} message will be used as a display name in reporting
+ * and should therefore be useful to a broad audience.
  *
- * @param values is used to supply context useful
- * for understanding the reason that condition had the value it did. For instance,
- * in an asertion that x > 5, it could be helpful to send the value of x so failing
- * cases can be better understood.
+ * @param {boolean} condition to be tested.
+ *
+ * @param {Object} values is used to supply context useful
+ * for understanding the reason that condition had the value it did.
+ * For instance, in an asertion that x > 5, it could be helpful to send
+ * the value of x so failing cases can be better understood.
  */
 export const Always = (
     message: string,
@@ -48,6 +50,132 @@ export const Always = (
     )
 }
 
+/**
+ * Assert that condition is true every time this function is called.
+ * Unlike the Always function, the test property spawned by AlwaysOrUnreachable
+ * will not be marked as failing if the function is never invoked.
+ * This test property will be viewable in the "Antithesis SDK: Always"
+ * group of the triage report.
+ *
+ * @param {string} message will be used as a display name in reporting
+ * and should therefore be useful to a broad audience.
+ *
+ * @param {boolean} condition to be tested.
+ *
+ * @param {Object} values is used to supply context useful
+ * for understanding the reason that condition had the value it did.
+ * For instance, in an asertion that x > 5, it could be helpful to send
+ * the value of x so failing cases can be better understood.
+ */
+export const AlwaysOrUnreachable = (
+    message: string,
+    condition: boolean,
+    values: JSONValue
+) => {
+    const location_info = newLocationInfo(offsetAPICaller)
+    assertImpl(
+        message,
+        condition,
+        values,
+        location_info,
+        was_hit,
+        optionally_hit,
+        expecting_true,
+        universal_test
+    )
+}
+
+/**
+ * Assert that condition is true at least one time that this function was called.
+ * The test property spawned by Sometimes will be marked as failing if this
+ * function is never called, or if condition is false every time that it is called.
+ * This test property will be viewable in the "Antithesis SDK: Sometimes" group
+ * of the triage report.
+ *
+ * @param {string} message will be used as a display name in reporting
+ * and should therefore be useful to a broad audience.
+ *
+ * @param {boolean} condition to be tested.
+ *
+ * @param {Object} values is used to supply context useful
+ * for understanding the reason that condition had the value it did.
+ * For instance, in an asertion that x > 5, it could be helpful to send
+ * the value of x so failing cases can be better understood.
+ */
+export const Sometimes = (
+    message: string,
+    condition: boolean,
+    values: JSONValue
+) => {
+    const location_info = newLocationInfo(offsetAPICaller)
+    assertImpl(
+        message,
+        condition,
+        values,
+        location_info,
+        was_hit,
+        must_be_hit,
+        expecting_true,
+        existential_test
+    )
+}
+
+/**
+ * Assert that a line of code is never reached.
+ * The test property spawned by Unreachable will be marked as failing if this
+ * function is ever called. This test property will be viewable in the
+ * "Antithesis SDK: Reachablity assertions" group of the triage report.
+ *
+ * @param {string} message will be used as a display name in reporting
+ * and should therefore be useful to a broad audience.
+ *
+ * @param {Object} values is used to supply context useful
+ * for understanding the reason that condition had the value it did.
+ * For instance, in an asertion that x > 5, it could be helpful to send
+ * the value of x so failing cases can be better understood.
+ */
+export const Unreachable = (message: string, values: JSONValue) => {
+    const location_info = newLocationInfo(offsetAPICaller)
+    assertImpl(
+        message,
+        true,
+        values,
+        location_info,
+        was_hit,
+        optionally_hit,
+        expecting_true,
+        reachability_test
+    )
+}
+
+/**
+ * Assert that a line of code is reached at least once.
+ * The test property spawned by Reachable will be marked as failing if
+ * this function is never called. This test property will be viewable
+ * in the "Antithesis SDK: Reachablity assertions" group of the
+ * triage report.
+ *
+ * @param {string} message will be used as a display name in reporting
+ * and should therefore be useful to a broad audience.
+ *
+ * @param {Object} values is used to supply context useful
+ * for understanding the reason that condition had the value it did.
+ * For instance, in an asertion that x > 5, it could be helpful to send
+ * the value of x so failing cases can be better understood.
+ */
+export const Reachable = (message: string, values: JSONValue) => {
+    const location_info = newLocationInfo(offsetAPICaller)
+    assertImpl(
+        message,
+        true,
+        values,
+        location_info,
+        was_hit,
+        must_be_hit,
+        expecting_true,
+        reachability_test
+    )
+}
 /** Unwrapped raw assertion access for custom tooling. Not to be called directly.
  */
 export const AssertRaw = (
